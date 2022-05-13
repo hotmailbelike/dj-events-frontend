a77ccad1170ed05f50113fe6adc160eec1888d7b
Abdullah Omar <fishilike.fish@gmail.com>
2022-05-13 23:14:35 +0600

    env: modified Layout component to have Header and Footer

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+	return (
+		<footer className={styles.footer}>
+			<p>Copyright &copy; DJ Events 2022</p>
+
+			<p>
+				<Link href={'/about'}>About This Project</Link>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
 import styles from '../styles/Layout.module.css';
 
 const Layout = ({ title, keywords, description, children }) => {
@@ -9,7 +11,9 @@ const Layout = ({ title, keywords, description, children }) => {
 				<meta name='description' content={description} />
 				<meta name='keywords' content={keywords} />
 			</Head>
+			<Header></Header>
 			<div className={styles.container}>{children}</div>
+			<Footer></Footer>
 		</div>
 	);
 };
